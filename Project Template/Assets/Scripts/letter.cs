eba46669dee0ff8ce4d39b2ff83d9615d24fd079
samisan2007 <samisan2007@gmail.com>
2020-03-21 21:42:31 +0200

    Letter and Blade collision, particles and explosion added

---
+++ Project Template/Assets/Scripts/letter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class letter : MonoBehaviour
+{
+
+    public GameObject explosion;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Blade")
+        {
+            Instantiate<GameObject>(explosion, transform.position, Quaternion.identity);
+        }
+    }
+
+
+}
