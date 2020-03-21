835611c93bc8b8a28c822d36cb6b19d29744f75e
samisan2007 <samisan2007@gmail.com>
2020-03-22 00:16:25 +0200

    fixes

--- Project Template/Assets/Scripts/gun_shoot.cs
+++ Project Template/Assets/Scripts/gun_shoot.cs
@@ -6,22 +6,29 @@ public class gun_shoot : MonoBehaviour
 {
     public Transform shootAnchor;
     public GameObject bullet;
+    public GameObject flash;
 
-
+    Animator anim;
     public OVRInput.Button shootButton;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (OVRInput.GetDown(shootButton, OVRInput.Controller.LTouch) || Input.GetButtonDown("Fire1") )
+        if (null != anim)
+        {
+            Debug.Log("empty animator");
+        }
+            if (OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger, OVRInput.Controller.LTouch)>0.3f || Input.GetButtonDown("Fire1") )
         {
             Instantiate(bullet, shootAnchor.position, shootAnchor.rotation);
+            Instantiate(flash, shootAnchor.position, shootAnchor.rotation);
+           anim.Play("pistolKick");
         }
     }
 }
